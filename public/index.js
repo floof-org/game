561ab4cb957187285097c3c9a0edd25061b80d16
Cryptverse <cryptverse.eth@gmail.com>
2026-05-09 11:36:15 +0000

    Revert "Flush GPU texture memory for inventory actively instead of waiting for garbage collector when closing."
    This reverts commit 7d7a473b7423a8202d00e48f536ad23f51408ca6.

--- public/index.js
+++ public/index.js
@@ -720,18 +720,8 @@ window.addEventListener("keydown", e => {
         }
     }
     if (e.key === "z" && !net.ChatMessage.showInput) {
-        const isNowActive = menu.classList.toggle("active");
-        if (isNowActive) {
-            drawInventory();
-        } else {
-            // Release GPU texture memory immediately rather than waiting for GC
-            net.state.petalElements?.forEach(({ icon }) => {
-                icon.width = 0;
-                icon.height = 0;
-            });
-            net.state.petalElements = [];
-            menu.innerHTML = "";
-        }
+        menu.classList.toggle("active");
+        drawInventory();
     }
 
     const petal = document.createElement("div");
