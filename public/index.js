a7752c703a6d16eb62f2c66fca688698a23d7fb4
Cryptverse <cryptverse.eth@gmail.com>
2026-05-20 23:56:18 +0000

    Ghost commit.

--- public/index.js
+++ public/index.js
@@ -927,7 +927,11 @@ function drawInventory() {
       .forEach(([petalIndex, count]) => {
         if (count <= 0) return;
 
-        const petalCanvas = getPetalIcon(Number(petalIndex), rarityIndex);
+        const petalCanvas = getPetalIcon(
+        Number(petalIndex),
+        rarityIndex,
+        "oneshot"
+        );
 
                 const icon = document.createElement("canvas");
 
@@ -3370,12 +3374,12 @@ function draw() {
 
   net.state._foundHover = false;
 
-    if (menu.classList.contains("active") && net.state.petalElements) {
-        net.state.petalElements.forEach(petal => {
-            const rect = petal.icon.getBoundingClientRect();
-            const menuRect = menu.getBoundingClientRect();
-            const mouseX = mouse.x;
-            const mouseY = mouse.y;
+  if (menu.classList.contains("active") && net.state.petalElements) {
+    net.state.petalElements.forEach((petal) => {
+      const rect = petal.icon.getBoundingClientRect();
+      const menuRect = menu.getBoundingClientRect();
+      const mouseX = mouse.x / window.devicePixelRatio;
+      const mouseY = mouse.y / window.devicePixelRatio;
 
             const visible =
                 rect.top >= menuRect.top &&
